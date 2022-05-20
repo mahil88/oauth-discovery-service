7d8e78a0439ee8494d61e45e3ce660f4c07f2f7a
mahilw <mahilw@cclk.lk>
2022-05-20 13:47:52 +0530

    oauth discovery service eureka server

---
+++ src/test/java/com/appdevblog/ws/api/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appdevblog.ws.api.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
